b5247dc64621eec36568d556b2a341aef7f076a0
ANRAx <ramdassaj@gmail.com>
2019-12-29 14:33:22 -0500

    add orm logic to config folder

---
+++ config/orm.js
@@ -0,0 +1,67 @@
+let connection = require("./connection.js");
+
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+let orm = {
+    all: function(tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    // vals = arry of values that will be saved to cols
+    // cols = columns to insert the values into 
+    create: function(tables, cols, vals, cb) {
+        let queryString = "INSERT INTO" + tables;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    // objColVals = columns and values to be updated
+    //  ex: {name: panther, sleepy: true}
+    update: function(table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
