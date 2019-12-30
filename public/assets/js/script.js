771d6fcff87461718e07e326a1073cd4429e07d5
ANRAx <ramdassaj@gmail.com>
2019-12-30 13:53:11 -0500

    add logic for submissions

---
+++ public/assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".devour-form").on("submit", function(event) {
+        event.preventDefault();
+
+        let sushi_id = $(this).children(".sushi_id").val();
+        console.log(sushi_id);;
+        $.ajax({
+            method: "PUT",
+            url: "/sushi/" + sushi_id
+        }).then(function(data) {
+            // reload page to display devoured burger in proper column
+            location.reload();
+        });
+        
+    });
+});
