4e663c454a275f6aa70a24ff535baf7e9fb1ec84
Sadhguru <ramdassaj@gmail.com>
2020-01-09 19:55:47 -0500

    update sec

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 let mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",
-    password: "Dharma4321420!",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "sushi_db"
 });
 
